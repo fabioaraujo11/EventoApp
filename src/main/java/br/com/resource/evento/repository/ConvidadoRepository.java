d2198185273d112f6476a6ff1cdb7a569acb3303
fabio <fabio@Astrofabio>
2019-12-02 17:37:16 -0300

    teste

---
+++ src/main/java/br/com/resource/evento/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.resource.evento.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.resource.evento.models.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
+
+}
